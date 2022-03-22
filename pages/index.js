d1964509a5e70a2d41e63e1457e2318563100a75
SeanAdami <seanwiz7@yahoo.com>
2022-03-21 20:33:50 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Hi" />
+        <p className="description">
+          </code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
 
